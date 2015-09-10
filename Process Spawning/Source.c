3292d606ed276686fae71ebcfb9275a02d1254f5
mullerpoint <gary@mullerpoint.com>
2015-09-10 16:57:22 -0700

    changed for loop control variable, removed parentPID variable and replaced it with getppid() where ever neccissary

--- Process Spawning/Source.c
+++ Process Spawning/Source.c
@@ -32,27 +32,27 @@ int main(int argv, char* argc[])
 {
 	//create variables
 	pid_t childPID = 0;
-	pid_t parentPID = getpid();
+	//pid_t parentPID = getpid();
 	int childCount;
 
 	//for loop to create child processes
-	for (childCount = 0; childCount < 3; childCount++)
+	for (childCount = 1; childCount <= 3; childCount++)
 	{
 		childPID = fork();//statement to create child process
 
 		if (childPID == 0)//if child process
 		{
-			printf("\tI am child #%d, my pid is %d; my parent's pid is %d\n", childCount+1, getpid(), parentPID); //print the statement that it is a child and the PID
-			if (childCount == 1)//if this is child two(2) replace it 
+			printf("\tI am child #%d, my pid is %d; my parent's pid is %d\n", childCount, getpid(), getppid()); //print the statement that it is a child and the PID
+			if (childCount == 2)//if this is child two(2) replace it 
 			{
 				execl(SPAWN_PROCESS_NAME, NULL); //if its process two replace the process with the execv() command
 			}//if for child 2 replace
 			
-			childCount = 3;
+			childCount = 4;
 		}//if child
 		else if (childPID > 0)//if parent process
 		{
-			printf("Parent, my pid is %d: Spawned child #%d whose pid is %d\n", parentPID, childCount+1, childPID);//print the statement that it is a parent and the PID
+			printf("Parent, my pid is %d: Spawned child #%d whose pid is %d\n", getpid(), childCount, childPID);//print the statement that it is a parent and the PID
 			childPID = 0; //reset PID to 0 so next child spawned doesnt think its a parent
 		}//elseif
 		else if (childPID < 0)//if there is an error in process creation of the child
