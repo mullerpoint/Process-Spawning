8e1a51336b40fc958b46f03f147aa92ad8d750f5
mullerpoint <gary@mullerpoint.com>
2015-09-10 15:28:23 -0700

    rewritten code for spec; not confirmed working

--- Process Spawning/Source.c
+++ Process Spawning/Source.c
@@ -5,32 +5,65 @@
 // CS 420 Assignment 1
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//A program that will spawn three child processes, print out the 
+//A program that will spawn three child processes, print out the Parent and Child PIDs
+//it will utilize fork() and execv() to spawn the processes 
+//specific implementations mean that this code will likely only run on RHEL
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Header files - Standard libraries and classes
+#include <unistd.h>
+#include <stdio.h>
 //
+
+//Global Variables and Defines
+#define SPAWN_PROCESS_NAME "child3.exe"
+//
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Main Declaration
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+int main(int argv, char* argc[])
+{
+	//create variables
+	pid_t childPID = 0;
+	pid_t parentPID = getpid();
+	int childCount;
 
-#include <unistd.h>
-#include <stdio.h>
-
-main(){
-	pid_t childPID1 = 0, childPID2, childPID3;
-
-	childPID = fork();
-
-
-	if (childPID == 0)
+	//for loop to create 
+	for (childCount = 0; childCount < 3; childCount++)
 	{
-		printf("I am child #1, my pid is %l; my parent's pid is 6461\n", getpid());
-		execv("notepad.exe");
+		childPID = fork();
+
+		if (childPID == 0)
+		{
+			if (childCount < 2)
+			{
+				printf("I am child #1, my pid is %l; my parent's pid is %l\n", getpid(), parentPID);
+			}
+			else
+			{
+				execv(SPAWN_PROCESS_NAME);
+			}
+
+		}
+		else if (childPID > 0)
+		{
+			printf("Parent, my pid is %l: Spawned child #1 whose pid is %l\n", parentPID, childPID);
+			childPID = 0;
+		}
+		else(childPID < 0)
+		{
+			printf("oops");
+		}
 	}
-	else if (childPID > 0)
-	{
-		printf("Parent, my pid is %l: Spawned child #1 whose pid is 6462\n", getpid(), childPID);
-	}
-	else(childPID < -1)
-	{
-		printf("oops");
-	}
+
+
 }
+
+
+
